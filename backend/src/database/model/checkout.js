58bf3c894d8bf3edb5bada29323ab809068ec81a
Miguel Rocha <miguelrocha.dev@gmail.com>
2022-03-17 23:16:03 +0000

    Challenge solved

---
+++ backend/src/database/model/checkout.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+const database = require('../index');
+
+const Checkout = database.define('checkout', {
+  id: {
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    allowNull: false,
+    primaryKey: true,
+  },
+  amount: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  type: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Checkout;
